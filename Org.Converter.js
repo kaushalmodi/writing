d3c0642a5993d04d313e988f8c12d169423e123f
Kaushal Modi <kaushal.modi@gmail.com>
2017-10-18 11:07:19 -0400

    Support Org headings

--- Org.Converter.js
+++ Org.Converter.js
@@ -841,41 +841,24 @@ else
 
         function _DoHeaders(text) {
 
-            // Setext-style headers:
-            //  Header 1
-            //  ========
-            //
-            //  Header 2
-            //  --------
-            //
-            text = text.replace(/^(.+)[ \t]*\n=+[ \t]*\n+/gm,
-                function (wholeMatch, m1) { return "<h1>" + _RunSpanGamut(m1) + "</h1>\n\n"; }
-            );
-
-            text = text.replace(/^(.+)[ \t]*\n-+[ \t]*\n+/gm,
-                function (matchFound, m1) { return "<h2>" + _RunSpanGamut(m1) + "</h2>\n\n"; }
-            );
-
-            // atx-style headers:
-            //  # Header 1
-            //  ## Header 2
-            //  ## Header 2 with closing hashes ##
+            //  * Header 1
+            //  ** Header 2
+            //  *** Header 3
             //  ...
-            //  ###### Header 6
+            //  ****** Header 10
             //
 
             /*
             text = text.replace(/
-                ^(\#{1,6})      // $1 = string of #'s
+                ^(\*{1,10})     // $1 = string of *'s
                 [ \t]*
                 (.+?)           // $2 = Header text
                 [ \t]*
-                \#*             // optional closing #'s (not counted)
                 \n+
             /gm, function() {...});
             */
 
-            text = text.replace(/^(\#{1,6})[ \t]*(.+?)[ \t]*\#*\n+/gm,
+            text = text.replace(/^(\*{1,10})[ \t]*(.+?)[ \t]*\n+/gm,
                 function (wholeMatch, m1, m2) {
                     var h_level = m1.length;
                     return "<h" + h_level + ">" + _RunSpanGamut(m2) + "</h" + h_level + ">\n\n";
